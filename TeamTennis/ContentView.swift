38fdc38246cc5709e9aa6a2a93410f40f780edae
SudhakarH <sudhakarh1999@gmail.com>
2020-08-27 21:03:10 +0530

    Initial Commit

---
+++ TeamTennis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeamTennis
+//
+//  Created by sudhakar H on 27/08/20.
+//  Copyright © 2020 sudhakar H. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
